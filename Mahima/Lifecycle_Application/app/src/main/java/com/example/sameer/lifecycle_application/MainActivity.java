36cd8b8ca05a70716099648bbea94b3137aefc6e
mahima-h <mahima.h@neorays.com>
2016-03-04 15:41:43 +0530

    Android App
    Activity_LifecycleApp

---
+++ Mahima/Lifecycle_Application/app/src/main/java/com/example/sameer/lifecycle_application/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.sameer.lifecycle_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"ON CREATE",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(MainActivity.this,"ON START",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        onPause();
+        Toast.makeText(MainActivity.this,"ON RESUME",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(MainActivity.this,"ON PAUSE",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(MainActivity.this,"ON STOP",Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(MainActivity.this, "ON RESTART", Toast.LENGTH_SHORT).show();
+       // super.onStart();
+    }
+}
