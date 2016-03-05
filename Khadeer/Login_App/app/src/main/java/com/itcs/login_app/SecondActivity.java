3b8f94bd7af84c7e407eaa79e7e33b0d38e59509
khadeer-b <khadeer.b@neorays.com>
2016-03-05 10:44:31 +0530

    first Login App
    Implementation of simple Login App

---
+++ Khadeer/Login_App/app/src/main/java/com/itcs/login_app/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.itcs.login_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by khussain on 2/19/2016.
+ */
+public class SecondActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_page);
+    }
+}
