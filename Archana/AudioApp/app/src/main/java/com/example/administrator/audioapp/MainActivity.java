03a8bf530ce7b7abdbb7235ebd350c91fd776e72
Archana-Manganahalli <archanasmbe@gmail.com>
2016-03-11 18:13:35 +0530

    Music player
    Application used to play music

---
+++ Archana/AudioApp/app/src/main/java/com/example/administrator/audioapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.administrator.audioapp;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    public void audioPlay(View view)
+    {
+        MediaPlayer mp = MediaPlayer.create(MainActivity.this,R.raw.sleep_away);
+        mp.start();
+    }
+}
