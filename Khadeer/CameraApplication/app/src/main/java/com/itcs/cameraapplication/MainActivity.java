8647354dad9eef32983f98758207f44bb3e5d8cf
khadeer-b <khadeer.b@neorays.com>
2016-03-15 17:35:16 +0530

    Android Camera Application
    implementation of camera in real device

---
+++ Khadeer/CameraApplication/app/src/main/java/com/itcs/cameraapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.itcs.cameraapplication;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        iv = (ImageView) findViewById(R.id.imageview1);
+        Button b = (Button) findViewById(R.id.btn1);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bm=(Bitmap)data.getExtras().get("data");
+        iv.setImageBitmap(bm);
+    }
+}
+
