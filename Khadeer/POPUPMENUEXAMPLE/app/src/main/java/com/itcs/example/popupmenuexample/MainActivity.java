44c43c67347dbec93036b6219dd09fd6e4d092d5
khadeer-b <khadeer.b@neorays.com>
2016-05-13 14:59:33 +0530

    Android PopUpMenu
    implementation of popup menu in android

---
+++ Khadeer/POPUPMENUEXAMPLE/app/src/main/java/com/itcs/example/popupmenuexample/MainActivity.java
@@ -0,0 +1,51 @@
+package com.itcs.example.popupmenuexample;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;;
+import android.view.View.OnClickListener;
+import android.widget.PopupMenu;
+import android.widget.PopupMenu.OnMenuItemClickListener;
+import android.widget.TextView;
+import android.app.Activity;
+public class MainActivity extends Activity implements OnClickListener, OnMenuItemClickListener
+{
+
+    private PopupMenu popupMenu;
+    private final static int ONE = 1;
+    private final static int TWO = 2;
+    private final static int THREE = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        popupMenu = new PopupMenu(this, findViewById(R.id.anchor));
+        popupMenu.getMenu().add(Menu.NONE, ONE, Menu.NONE, "Item 1");
+        popupMenu.getMenu().add(Menu.NONE, TWO, Menu.NONE, "Item 2");
+        popupMenu.getMenu().add(Menu.NONE, THREE, Menu.NONE, "Item 3");
+        popupMenu.setOnMenuItemClickListener(this);
+        findViewById(R.id.anchor).setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        popupMenu.show();
+    }
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        TextView tv = (TextView) findViewById(R.id.selection);
+        switch (item.getItemId()) {
+            case ONE:
+                tv.setText("ONE");
+                break;
+            case TWO:
+                tv.setText("TWO");
+                break;
+            case THREE:
+                tv.setText("THREE");
+                break;
+        }
+        return false;
+    }
+}
