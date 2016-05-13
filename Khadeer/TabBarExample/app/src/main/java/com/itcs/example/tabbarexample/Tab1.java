26235ccc61a34b98cea6989169b14005532a6d3f
khadeer-b <khadeer.b@neorays.com>
2016-05-13 14:41:21 +0530

    Android Tab bar App
    Implementing tab switching in android.

---
+++ Khadeer/TabBarExample/app/src/main/java/com/itcs/example/tabbarexample/Tab1.java
@@ -0,0 +1,19 @@
+package com.itcs.example.tabbarexample;
+
+/**
+ * Created by khussain on 5/10/2016.
+ */
+import android.os.Bundle;
+import android.app.Activity;
+
+
+public class Tab1 extends Activity {
+
+    /** Called when the activity is first created. */
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab1);
+    }
+}
